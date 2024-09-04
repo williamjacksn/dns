0f40cc7bbaa740db145fc23c9ba3423da7317aae
William Jackson <565174+williamjacksn@users.noreply.github.com>
2024-09-04 13:52:58 -0500

    Add code.subtlecoolness.com

--- gen.py
+++ gen.py
@@ -100,6 +100,7 @@ for hostname, ip in tailscale_hosts.items():
 ### cnames for tailscale hosts
 
 tailscale_cnames = {
+    'code': 'dagobah',
     'music': 'dagobah',
     'papers': 'dagobah',
     'photos': 'dagobah',
