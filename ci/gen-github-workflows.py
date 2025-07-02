97c25a26d7a4e716102926bd15e058c035bcf01d
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-07-01 19:19:21 -0500

    Change workflow, job, and step names

--- ci/gen-github-workflows.py
+++ ci/gen-github-workflows.py
@@ -1,9 +1,10 @@
 import gen
 
 gen_py = {"branches": ["main"], "paths": ["gen.py"]}
+cf_env = {"CLOUDFLARE_TOKEN": "${{ secrets.cloudflare_token }}"}
 
 workflow = {
-    "name": "Compile octoDNS settings and deploy to production",
+    "name": "Deploy DNS configuration",
     "on": {
         "pull_request": gen_py,
         "push": gen_py,
@@ -18,23 +19,23 @@ workflow = {
         },
     },
     "jobs": {
-        "compile": {
-            "name": "Compile and deploy octoDNS settings",
+        "deploy": {
+            "name": "Deploy DNS configuration",
             "runs-on": "ubuntu-latest",
             "steps": [
                 {"name": "Check out the repository", "uses": "actions/checkout@v4"},
                 {"name": "Install uv", "run": "sh ci/install-uv.sh"},
-                {"name": "Compile octoDNS settings", "run": "sh ci/gen-config.sh"},
+                {"name": "Generate DNS configuration", "run": "sh ci/gen-config.sh"},
                 {
-                    "name": "Sync dry run",
+                    "name": "Dry run deploy",
                     "if": "github.even_name == 'pull_request' || (github.event_name == 'workflow_dispatch' && inputs.dry_run)",
-                    "env": {"CLOUDFLARE_TOKEN": "${{ secrets.cloudflare_token }}"},
+                    "env": cf_env,
                     "run": "sh ci/dry-run.sh",
                 },
                 {
-                    "name": "Sync for real",
+                    "name": "Live deploy",
                     "if": "github.even_name == 'push' || (github.event_name == 'workflow_dispatch' && ! inputs.dry_run)",
-                    "env": {"CLOUDFLARE_TOKEN": "${{ secrets.cloudflare_token }}"},
+                    "env": cf_env,
                     "run": "sh ci/deploy-dns.sh",
                 },
             ],
